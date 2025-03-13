9527152a86bb45749cef4b109da92db6257e56c1
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2025-03-13 13:15:02 +0100

    good aplication

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 
+from pdf2image import convert_from_path
 from pathlib import Path
 from dotenv import dotenv_values
 import streamlit as st
 from openai import OpenAI
 import json
 import base64
-import instructor
 from pydantic import BaseModel
-from pdf2image import convert_from_path
+
 from PIL import Image
+import instructor
 
 # Załadowanie zmiennych środowiskowych
 env = dotenv_values(".env")
@@ -19,23 +20,25 @@ ALL_FILES_AFTER_ANALISIS_PATH = FILES_FOR_ANALISIS_PATH / "all_files_after_anali
 FILES_REJECTED_PATH = FILES_FOR_ANALISIS_PATH / "applications_rejected"
 FILES_ACCEPT_PATH = FILES_FOR_ANALISIS_PATH / "applications_accept"
 pdf_path = FILES_FOR_ANALISIS_PATH / "wzor_fiszki_wypełniona.pdf"
-output_path = FILES_FOR_ANALISIS_PATH / "plik_skonwertowany.png"
+#output_path = FILES_FOR_ANALISIS_PATH / "wzor_fiszki_wypełniona.png"
+output_path = FILES_FOR_ANALISIS_PATH / "wzor_fiszki_wypełniona1.png"
+#output_path = FILES_FOR_ANALISIS_PATH / "fiszka.png"
 
 # Tworzenie katalogów, jeśli nie istnieją
 for path in [FILES_FOR_ANALISIS_PATH, FILES_ACCEPT_PATH, FILES_REJECTED_PATH, ALL_FILES_AFTER_ANALISIS_PATH]:
     path.mkdir(exist_ok=True)
 
 # Konfiguracja OpenAI API
-api_key = env.get("OPENAI_API_KEY")
-if not api_key:
-    st.warning("Brak klucza API OpenAI w pliku .env")
-openai_client = OpenAI(api_key=api_key)
+# OpenAI API key protection
+if not st.session_state.get("openai_api_key"):
+    if "OPENAI_API_KEY" in env:
+        st.session_state["openai_api_key"] = env["OPENAI_API_KEY"]
 
-# Obsługa klucza API w sesji
-if "openai_api_key" not in st.session_state:
-    st.session_state["openai_api_key"] = api_key or st.text_input("Klucz API", type="password")
-    if st.session_state["openai_api_key"]:
-        st.rerun()
+    else:
+        st.info("Dodaj swój klucz API OpenAI aby móc korzystać z tej aplikacji")
+        st.session_state["openai_api_key"] = st.text_input("Klucz API", type="password")
+        if st.session_state["openai_api_key"]:
+            st.rerun()
 
 if not st.session_state.get("openai_api_key"):
     st.stop()
@@ -44,27 +47,31 @@ st.success("Aplikacja gotowa do użycia")
 st.write("Aplikacja do weryfikacji plików")
 
 
+def get_openai_client():
+    return OpenAI(api_key=st.session_state["openai_api_key"])
+
+
 def convert_pdf_to_png(pdf_path, output_path):
     """ Konwersja PDF na jeden obraz PNG zawierający wszystkie strony """
     if not pdf_path.exists():
         st.error(f"Plik PDF {pdf_path} nie istnieje.")
         return None
-    
+
     images = convert_from_path(pdf_path, dpi=300)
     if not images:
         st.error("Nie udało się przekonwertować PDF na obraz.")
         return None
-    
+
     widths, heights = zip(*(img.size for img in images))
     total_width = max(widths)
     total_height = sum(heights)
-    
+
     combined_image = Image.new('RGB', (total_width, total_height))
     y_offset = 0
     for img in images:
         combined_image.paste(img, (0, y_offset))
         y_offset += img.size[1]
-    
+
     combined_image.save(output_path, format="PNG")
     return output_path
 
@@ -74,13 +81,11 @@ def prepare_image_for_open_ai(output_path):
     if not output_path.exists():
         st.error(f"Plik {output_path} nie istnieje.")
         return None
-    
+
     with open(output_path, "rb") as f:
         image_data = base64.b64encode(f.read()).decode("utf-8")
-    
+
     return f"data:image/png;base64,{image_data}"
-
-
 
 
 if st.button("Przekonweruj plik pdf do png"):
@@ -101,66 +106,57 @@ if st.button("Weryfikuj nowe pliki"):
         )
     else:
         st.warning("Plik nie istnieje w katalogu files_for_analysis.")
-        
-  
- 
 
 
-  
-  
-        
-# class ImageInfo(BaseModel):
-#             First_Name: str
-#             Surname: str
-#             Sex: str
-#             Email: str
-#             Phone: int
-#             Punkt_8: bool
-#             Punkt_9: bool
-#             Punkt_10: bool
-            
-#instructor_openai_client = instructor.from_openai(openai_client)
 
-if st.button("Przeanalizuj plik"):
+class ImageInfo(BaseModel):
+    First_Name: str
+    Surname: str
+    Sex: str
+    Email: str
+    Punkt_9: bool
+    Punkt_10: bool
+    Punkt_11: bool
+
+
+
+openai_client = get_openai_client()
+instructor_openai_client = instructor.from_openai(openai_client)
+  
+
+
+if st.button("Przeanalizuj plik") and 'instructor_openai_client' in locals():
     if output_path.exists():
-
-
+      
        
-            
-            
-        
-        image_data = prepare_image_for_open_ai(output_path)
-         
-        if image_data:
-            
-           # info = instructor_openai_client = instructor.from_openai(openai_client)
-           
-            response = openai_client.chat.completions.create(
-                model="gpt-4o",
-                response_model=ImageInfo,
-                messages=[
-                    {
-                        "role": "user",
-                       "content": [
-                            {
-                                "type": "text",
-                                "text": "dla punktu 8 i 10 odpowiedz jest zaznaczona czarnym punktem w odległości około 3 mm obok tak lub nie",
-                            },
-                            {
-                                "type": "image_url",
-                                "image_url": {
-                                    "url": prepare_image_for_open_ai(output_path),
-                                    "detail": "high"
+       
+            try:
+                gas_bill = instructor_openai_client.chat.completions.create(
+                    model="gpt-4o",
+                    response_model=ImageInfo,
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "text",
+                                    "text": "Pobierz dane",
                                 },
-                            },
-                        ],
-                    },
-                ],
-            )
-            #st.write(ImageInfo)
-            print(response.choices[0].message.content)
+                                {
+                                    "type": "image_url",
+                                    "image_url": {
+                                        "url": prepare_image_for_open_ai(output_path),
+                                        "detail": "high"
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                )
 
-        else:
-            st.warning("Nie udało się przygotować obrazu do analizy.")
+                st.write(gas_bill)
+                st.write(output_path)
+            except Exception as e:
+                st.error(f"Błąd podczas uzyskiwania odpowiedzi z OpenAI: {e}")
     else:
-        st.warning("Plik nie istnieje.")
+        st.warning("Plik nie istnieje w katalogu files_for_analysis.")
